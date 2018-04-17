79b30e81e31b180c3dc650c7bae7e2539c552bb9
Miguel Boanova <mgl.deadly@gmail.com>
2018-04-17 18:06:28 -0300

    Primeira query com token feita

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,31 +1,40 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import RestaurantContainer from './RestaurantContainer'
-import { HashRouter, Switch, Route } from 'react-router-dom'
+import {HashRouter, Switch, Route} from 'react-router-dom'
 import Sidebar from '../components/Sidebar'
 import './App.scss'
 import LoginContainer from './LoginContainer';
 import ApolloClient from 'apollo-boost'
-import { ApolloProvider } from 'react-apollo'
-import { createHttpLink } from 'apollo-link-http';
-import { setContext } from 'apollo-link-context';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import {ApolloProvider} from 'react-apollo'
+import {createHttpLink} from 'apollo-link-http';
+import {setContext} from 'apollo-link-context';
+import {InMemoryCache} from 'apollo-cache-inmemory';
 const client = new ApolloClient({
   uri: "https://godinner-backend.herokuapp.com/graphql"
 });
 
-// TODO Arrumar para fazer requisição autenticado.
 const secret = new ApolloClient({
-  uri: 'https://godinner-backend.herokuapp.com/graphql/secret',
-  cache: InMemoryCache()
-});
+  uri: "https://godinner-backend.herokuapp.com/graphql/secret",
+  fetchOptions: {
+    fetch: true,
+  },
+  request: async (operation) => {
+    const token = await localStorage.getItem('token');
+    operation.setContext({
+      headers: {
+        authorization: token
+      }
+    });
+  }
+})
 
 class App extends Component {
-  state = { visible: true }
+  state = {visible: true}
 
-  toggleVisibility = () => this.setState({ visible: !this.state.visible })
+  toggleVisibility = () => this.setState({visible: !this.state.visible})
 
   render () {
-    const { visible } = this.state
+    const {visible} = this.state
     return (
       <HashRouter>
         <Route render={(location) => (
--- src/containers/RestaurantContainer/index.js
+++ src/containers/RestaurantContainer/index.js
@@ -1,30 +1,92 @@
 import React from 'react';
 import gql from 'graphql-tag'
-import { Query } from 'react-apollo'
+import {Query} from 'react-apollo'
 import './index.scss'
 
 const restaurant = gql`
-query restaurant ($id: ID!){
+query restaurant ($id: Int!){
   restaurant(id: $id) {
-    name
-  }
+    id
+		name
+		subname
+		location
+		number
+		phone_number
+		cellphone_number
+		cpnj
+		avatar_url
+		description
+		user {
+			id
+			name
+			email
+		}
+		stars {
+			id
+			star
+			client {
+				id
+				name
+				lastname
+			}
+		}
+		bookings {
+			id
+			client {
+				id
+				name
+			}
+		}
+		posts {
+			id
+			likes {
+				user{
+					id
+					name
+				}
+				liked
+			}
+			comments {
+				
+				user {
+					id
+					name
+				}
+				comment
+			}
+		}
+		menus { 
+			id
+			type
+			menu_options {
+				price
+				id
+				name
+				ingredients
+			}
+		}
+		tables {
+			id
+			state
+		}
+	}
 }
 `
 
 class RestaurantContainer extends React.Component {
-  constructor() {
+  constructor () {
     super();
   }
 
   render () {
     return this.props.match.params.param === 'meuperfil' ? (
       <Query query={restaurant} variables={{id: localStorage['id']}}>
-        {({ loading, error, data }) => {
+        {({loading, error, data}) => {
           if (loading) return "Loading..."
           if (error) return `Error! ${error}`
-
+          console.log(data)
           return (
-            data.restaurant[0].name
+            <p>{data.restaurant[0].name}</p>
           )
         }}
       </Query>
