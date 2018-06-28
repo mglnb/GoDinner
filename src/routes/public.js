4bbc84c1d6c8c850143c4da2cbdd1f777f7c5b9e
Miguel Boanova <migboano@publicisgroupe.net>
2018-06-28 14:32:48 -0300

    Socket finalizado

--- src/routes/public.js
+++ src/routes/public.js
@@ -1,9 +1,14 @@
 import LoginContainer from '../containers/LoginContainer';
+import ClientFeed from '../components/ClientFeed/index.js'
 
 export default [
   {
     exact: true,
     path: '/',
     component: LoginContainer
+  }, {
+    exact: true,
+    path: '/client',
+    component: ClientFeed
   }
 ]
